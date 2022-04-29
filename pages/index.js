e84ec3f26ffeae9a7919bc7e8409b56596fc67f9
Shivom <shvomsrivastava@gmail.com>
2022-04-29 22:48:40 +0530

    add krishna image to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 export default function Home({ className }) {
+    const bgImage =
+        'https://images.unsplash.com/photo-1609133799670-2c2a82f3624c?&auto=format&fit=crop&w=1170&q=80'
     return (
-        <div className={className}>
-            <h1 className="text-xl font-bold">
-                Mahabharat Image will be placed here
-            </h1>
-        </div>
+        <div
+            className={className}
+            style={{
+                background: `url(${bgImage})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+            }}
+        ></div>
     )
 }
