bb13c7dde3529df776916607b2c3813f81c5c86e
Shivom <shvomsrivastava@gmail.com>
2022-04-29 23:38:13 +0530

    ✨ added jumper component

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import useFetch from '../util/useFetch'
 
 const Sidebar = ({ openModal }) => {
@@ -23,21 +24,10 @@ const Sidebar = ({ openModal }) => {
                     </>
                 ) : (
                     <>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quasi minus amet sint unde dicta quos natus quibusdam
-                        assumenda? Mollitia dignissimos labore nulla quidem qui
-                        perspiciatis accusamus magnam eos unde magni aut
-                        nesciunt voluptates odio illum iusto iure quis nostrum,
-                        deserunt ex cupiditate assumenda dolores alias. Sunt
-                        magni soluta fugit ducimus. Ipsum distinctio adipisci
-                        explicabo optio nulla voluptatum! Fugiat nostrum,
-                        consequuntur laboriosam esse quod expedita quaerat
-                        suscipit quasi nemo? Corporis tenetur reprehenderit
-                        velit illum fuga excepturi beatae ullam suscipit numquam
-                        veritatis minus, similique voluptatibus dolorum quasi
-                        expedita animi repellat esse obcaecati nisi dolorem,
-                        soluta repudiandae neque voluptas ipsam. Sunt, enim
-                        provident?
+                        Bhagavad Gita, also known as the Gita - "The Song of
+                        God" is a practical guide to one's life that guides one
+                        to re-organise their life, achieve inner peace and
+                        approach the Supreme (the Ultimate Reality).
                     </>
                 )}
             </p>
@@ -45,8 +35,18 @@ const Sidebar = ({ openModal }) => {
                 onClick={() => router.push('/explore')}
                 className="btn mb-auto"
             >
-                Explore
+                explore
             </button>
+            <div className="flex gap-2 items-center text-2xl">
+                <label htmlFor="cars" className="">
+                    choose your language
+                </label>
+                <select className="w-48 rounded-xl" name="cars" id="cars">
+                    <option value="en">English</option>
+                    <option value="hi">Hindi</option>
+                </select>
+            </div>
+            <Jumper />
             <ul className="flex justify-between gap-2">
                 {['about', 'api', 'credits', 'contribute'].map((to) => (
                     <LinkItem to={to} key={to} openModal={openModal} />
@@ -61,5 +61,48 @@ const LinkItem = ({ to, openModal }) => (
         {to}
     </li>
 )
+
+const Jumper = () => {
+    const router = useRouter()
+    const [chapter, setChapter] = useState(null)
+    const [verse, setVerse] = useState(null)
+    const handler = () => {
+        if (chapter && verse) {
+            if (parseInt(chapter) <= 18) {
+                router.push(`/chapter/${chapter}/verse/${verse}`)
+            }
+        }
+    }
+    return (
+        <div className="">
+            <span className="mb-1">
+                Jump to a verse {chapter}
+                {verse}
+            </span>
+            <div className="flex gap-2">
+                <input
+                    type="number"
+                    placeholder="Chapter Number"
+                    className="rounded-xl indent-3"
+                    value={chapter}
+                    onChange={(e) => {
+                        if (chapter > 18) setChapter(1)
+                        else setChapter(e.target.value)
+                    }}
+                />
+                <input
+                    type="number"
+                    className="rounded-xl indent-3"
+                    placeholder="Verse"
+                    value={verse}
+                    onChange={(e) => setVerse(e.target.value)}
+                />
+                <button onClick={handler} className="btn">
+                    →
+                </button>
+            </div>
+        </div>
+    )
+}
 
 export default Sidebar
